8d05db596510e8e930d87f4ea26e988fab077c35
create1666 <create1666@gmail.com>
2021-10-15 18:55:24 +0100

    grant User Access permission

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
-import React from 'react'
+import React, { useState } from 'react'
 import Login from './components/LOGIN/Login';
+import  { useEffect } from 'react'
+import { getTokenUrl } from './Utils';
+
 
 const App = () => {
+  const [token, setToken] = useState(null)
+useEffect(() => {
+   const hash = getTokenUrl()
+   window.location.hash = ""
+   const _token = hash.access_token
+
+   if(_token){
+     setToken(_token)
+   }
+   console.log("I HAVE A TOKEN=>", token)
+}, [])
   return (
     <div className="app">
-      <h1>Login Page</h1>
-      <Login/>
+     
+    {token? <h1>Successfully logged in</h1> :  <Login/>}
     </div>
   )
 }
