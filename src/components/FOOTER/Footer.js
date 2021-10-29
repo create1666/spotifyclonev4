91b014f4f558d519da34a1ba06fb5cd8d2b12b59
create1666 <create1666@gmail.com>
2021-10-29 19:19:41 +0100

    fixes

--- src/components/FOOTER/Footer.js
+++ src/components/FOOTER/Footer.js
@@ -1,24 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Footer.css";
 import ShuffleIcon from "@mui/icons-material/Shuffle";
 import {
   PlayCircleOutline,
+  PlaylistPlayOutlined,
   Repeat,
   SkipNext,
   SkipPrevious,
+  VolumeDownOutlined,
 } from "@mui/icons-material";
+import { Grid, Slider } from "@mui/material";
+import { Box } from "@mui/system";
+import classNames from "classnames";
 const Footer = () => {
+  const [colorChange, setColor] = useState("");
   return (
     <div className="footer">
-      <div className="footer__lleft">Album and song details</div>
+      <div className="footer__left">
+        <img
+          src="https://www.currentschoolnews.com/wp-content/uploads/2021/06/Gear-RIP-Google-Music-1194411695.jpg"
+          alt=""
+        />
+
+        <div className="song__title">
+          <h4>Ckay - Love nwantintin</h4>
+          <p>Buju X Timayah - cold outside</p>
+        </div>
+      </div>
       <div className="footer__center">
-        <ShuffleIcon className="footer__shuffle" />
+        <ShuffleIcon
+          className={classNames("footer__shuffle", {
+            "--green": colorChange === "green",
+          })}
+          onClick={() => setColor("green")}
+        />
         <SkipPrevious className="footer__skipPrevious" />
         <PlayCircleOutline className="footer__playCircleOutline" />
-        <SkipNext className="footer__skipNext" />
-        <Repeat className="footer__repeat" />
+        <SkipNext className="footer__skipPrevious" />
+        <Repeat
+          className={`footer__repeat ${colorChange === "red" && "--red"}
+           
+          `}
+          onClick={() => setColor("red")}
+        />
       </div>
-      <div className="footer__right">Volume Controls</div>
+      <div className="footer__right">
+        {" "}
+        <PlaylistPlayOutlined />
+        <VolumeDownOutlined />
+        <Grid>
+          <Box width={200}>
+            <Slider marks min={10} max={110} />
+          </Box>
+        </Grid>
+      </div>
     </div>
   );
 };
